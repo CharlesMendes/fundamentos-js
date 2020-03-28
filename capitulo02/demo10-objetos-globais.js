c2da8fe4331fc00cb388be2f415911d46ec03102
Charles Mendes <contato@charlesmendes.com>
2020-03-28 16:50:16 -0300

    global no js

---
+++ capitulo02/demo10-objetos-globais.js
@@ -0,0 +1,13 @@
+console.log('keys', Object.keys({
+  nome: 'Charles'
+}))
+
+console.log('values', Object.values({
+  nome: 'Charles'
+}))
+
+console.log('now', Date.now())
+console.log('random', Math.random())
+
+// global
+console.log('global', global)
